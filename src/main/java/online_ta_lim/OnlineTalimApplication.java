0aac7e5054c048cba6e905ef4d28bde2c476d301
stranger <ismoiljonovabdulaziz97@gmail.com>
2024-08-28 12:11:51 +0500

    Initial commit

---
+++ src/main/java/online_ta_lim/OnlineTalimApplication.java
@@ -0,0 +1,13 @@
+package online_ta_lim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTalimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTalimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online_ta_lim/OnlineTalimApplicationTests.java
@@ -0,0 +1,13 @@
+package online_ta_lim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTalimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
